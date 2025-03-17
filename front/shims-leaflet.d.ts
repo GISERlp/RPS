2069c628ae8f1297158575a3ee0d0c3dbceb55f8
GISERlp <148303779+GISERlp@users.noreply.github.com>
2025-03-18 02:35:32 +0800

    布局更新
    添加了地图以及相关切换控件

---
+++ front/shims-leaflet.d.ts
@@ -0,0 +1,2 @@
+// filepath: d:\github_desktop\RPS\front\src\shims-leaflet.d.ts
+declare module 'leaflet';
--- front/src/components/homepage/homepage.vue
+++ front/src/components/homepage/homepage.vue
@@ -1,58 +1,104 @@
 <template>
-  <div class="homepage">
-    <header class="header">
-      <h1>头部模块</h1>
-    </header>
-    <main class="main-content">
-      <div class="left-side">左侧模块</div>
-      <div class="center-content">主内容模块</div>
-    </main>
-    <!-- <footer class="footer">
-      <p>底部模块</p>
-    </footer> -->
+  <div class="common-layout">
+    <el-container>
+      <el-header> 径流预测系统 </el-header>
+      <el-container>
+        <!-- <el-aside width="200px">Aside</el-aside> -->
+        <el-main>
+          <div id="map" class="map"></div>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
 export default {
   name: "Homepage",
+  data() {
+    return {
+      map: null,
+      url: "https://iserver.supermap.io",
+      Tianditu: null,
+      China: null,
+      ChinaDark: null,
+      Tianditu_road: null,
+      baseMaps: {
+        Tianditu: null,
+        China: null,
+        ChinaDark: null,
+        Tianditu_road: null,
+      },
+    };
+  },
+  mounted() {
+    this.initMap();
+  },
+  methods: {
+    // 使用id为map的div容器初始化地图
+    initMap() {
+      this.China = new L.supermap.TiledMapLayer(
+        this.url + "/iserver/services/map-china400/rest/maps/China",
+        { noWrap: true }
+      );
+      this.ChinaDark = new L.supermap.TiledMapLayer(
+        this.url + "/iserver/services/map-china400/rest/maps/ChinaDark",
+        { noWrap: true }
+      );
+      this.Tianditu = new L.supermap.TiandituTileLayer({
+        layerType: "img",
+        key: "1d109683f4d84198e37a38c442d68311",
+      });
+      this.Tianditu_road = new L.supermap.TiandituTileLayer({
+        layerType: "img",
+        isLabel: true,
+        key: "1d109683f4d84198e37a38c442d68311",
+      });
+      this.baseMaps = {
+        Tianditu: this.Tianditu,
+        China: this.China,
+        ChinaDark: this.ChinaDark,
+        Tianditu_road: this.Tianditu_road,
+      };
+
+      this.map = L.map("map", {
+        center: [24.886566, 102.830513], // 中心位置
+        zoom: 11, // 缩放等级
+        crs: L.supermap.CRS.TianDiTu_Mercator, // 使用天地图坐标系
+        layers: [this.Tianditu, this.Tianditu_road], // 默认底图
+      });
+
+      L.control.layers(this.baseMaps).addTo(this.map);
+      // 添加天地图图层
+    },
+  },
 };
 </script>
 
 <style scoped>
-.homepage {
+.map {
+  height: 100%;
+  width: 100%;
+}
+.el-header {
+  background-color: #a0cfff; /* 浅灰色背景 */
+  height: 10vh; /* 占页面高度10% */
   display: flex;
-  flex-direction: column;
-  height: 100vh;
+  align-items: center;
+  justify-content: center;
+  font-size: x-large;
+  font-family: "Microsoft YaHei", sans-serif; /* 设置字体为微软雅黑 */
+  font-weight: bold; /* 加粗 */
 }
 
-.header {
-  background-color: #ffcccc;
-  padding: 20px;
-  text-align: center;
+.el-aside {
+  background-color: #337ecc; /* 浅蓝色背景 */
+  height: 90vh; /* 占页面高度90% */
 }
 
-.main-content {
-  display: flex;
-  flex: 1;
-  background-color: #ccffcc;
-}
-
-.left-side {
-  flex: 1;
-  background-color: #e0e0e0;
-  padding: 20px;
-}
-
-.center-content {
-  flex: 3;
-  background-color: #ffffff;
-  padding: 20px;
-}
-
-.footer {
-  background-color: #ccccff;
-  padding: 20px;
-  text-align: center;
+.el-main {
+  padding: 0px;
+  background-color: #d9ecff; /* 浅黄色背景 */
+  height: 90vh; /* 占页面高度90% */
 }
 </style>
